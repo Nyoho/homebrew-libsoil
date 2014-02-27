e71acc413b42df1677f933b18e8ef5c96eed980c
北䑓如法 <algebraicallyClosedField@gmail.com>
2014-02-27 15:44:40 +0900

    Changed the path of include files
    To use not #include <SOIL.h> but <SOIL/SOIL.h>

--- libsoil.rb
+++ libsoil.rb
@@ -11,19 +11,39 @@ class Libsoil < Formula
   end
 
   def install
-    system "make", "lib"
-    include.install "SOIL.h"
-    lib.install "libSOIL.a"
-    lib.install "libSOIL.dylib"
+    system "make lib"
+    system "make install"
   end
 end
 
 __END__
 diff --git a/Makefile b/Makefile
-index 6844aa6..8c1a093 100755
+index 6844aa6..630a698 100755
 --- a/Makefile
 +++ b/Makefile
-@@ -30,7 +30,7 @@ lib: $(OFILES)
+@@ -1,7 +1,7 @@
+ MAKE = make
+ CC = gcc
+-INSTALL_FILE = install -p -o root -g admin -m 644
+-INSTALL_DIR = install -o root -g admin -d
++INSTALL_FILE = install -p -m 644
++INSTALL_DIR = install -d
+ LN = ln -s
+ RM = rm -fv
+ CFLAGS += -c -O2 -Wall
+@@ -17,8 +17,9 @@ HFILES = SOIL.h image_DXT.h image_helper.h \
+   stbi_DDS_aug.h stbi_DDS_aug_c.h stb_image_aug.h
+ AFILE = libSOIL.a
+ DYLIBFILE = libSOIL.dylib
+-INCLUDEDIR = opt/local/include/SOIL
+-LIBDIR = opt/local/lib
++DESTDIR = /$HOMEBREW_PREFIX
++INCLUDEDIR = include/SOIL
++LIBDIR = lib
+ 
+ all: $(OFILES) lib
+ 
+@@ -30,7 +31,7 @@ lib: $(OFILES)
  	ar -cvq $(LIBNAME).a $(OFILES)
  	# create shared library
  	gcc -dynamiclib -o $(DYLIBFILE) $(OFILES) $(LDFLAGS)\
